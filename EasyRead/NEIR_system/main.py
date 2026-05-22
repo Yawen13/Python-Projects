9bcc8b806e3fb39c85609a64ba93bd8468724913
Joseph <zhuyawen12@gmail.com>
2026-05-22 14:11:38 +0800

    Delete EasyRead directory

--- EasyRead/NEIR_system/main.py
+++
@@ -1,9 +0,0 @@
-from database import init_db
-from ui import DeviceSystemUI
-import tkinter as tk
-
-if __name__ == "__main__":
-    init_db()
-    root = tk.Tk()
-    app = DeviceSystemUI(root)
-    root.mainloop()
